067040daa564b6f53e4100c73d4ec1b77d2e4d22
RISMALUKMANHAKIM <risma_186@smkassalaambandung.sch.id>
2021-11-30 11:39:12 +0700

    BELAJAR LOOPING BOSKUU

---
+++ looping/latihan/bilangan_genap.php
@@ -0,0 +1,6 @@
+<?php
+
+for($i = 10; $i < 55; $i+2){
+    echo "<h2>Ini Bilangan Genap $i</h2>";
+}
+?>
---
+++ looping/latihan/pemangkatan.php
@@ -0,0 +1,6 @@
+<?php
+
+for($i = 2; $i <= 256; $i*= 2){
+    echo "<br>Ini pemangkatan ke- $i</br>";
+}
+?>
